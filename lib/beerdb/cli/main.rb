ceaa6dbc9a919cafa0c863efeb39defb2a5ab355
Gerald Bauer <gerald.bauer@gmail.com>
2014-10-14 09:53:47 +0200

    up load

--- lib/beerdb/cli/main.rb
+++ lib/beerdb/cli/main.rb
@@ -150,6 +150,32 @@ end  # command setup
 
 
 
+desc 'Load beer fixtures'
+arg_name 'NAME'   # multiple fixture names - todo/fix: use multiple option
+command [:load, :l] do |c|
+
+  c.desc 'Delete all beer data records'
+  c.switch [:delete], negatable: false 
+
+  c.action do |g,o,args|
+
+    connect_to_db( opts )
+    
+    BeerDb.delete! if o[:delete].present?
+
+    reader = BeerDb::Reader.new( opts.data_path )
+
+    args.each do |arg|
+      name = arg     # File.basename( arg, '.*' )
+      reader.load( name )
+    end # each arg
+
+    puts 'Done.'
+  end
+end # command load
+
+
+
 ## fix/todo: add server alias (serve/server)
 
 desc 'Start web service (HTTP JSON API)'
